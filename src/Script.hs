2a8fe622303023c7138a606d7397e9c5e32ffb1e
Alexey Gaziev <alex.gaziev@gmail.com>
2014-11-20 01:28:29 +0800

    Modify script

--- src/Script.hs
+++ src/Script.hs
@@ -3,9 +3,7 @@ import System.IO
 
 main :: IO ()
 main = do
-    -- handle <- openFile "../test/test_file.giml" ReadMode
-    -- contents <- hGetContents handle
-    let contents = "# shit yeah\n:num: shitNum\n1234.432\n\n:list: someList\n# shit shit\n1, 2, 3, 4\n5, 6, 7, 8\n\n:num: someNum\n1234\n\n:text: someText\ntext text text\ntext text\n"
-    let parsed = parseLines (lines contents) Nothing
-    -- hClose handle
+    contents <- readFile "../test/test_file.giml"
+    let content = lines contents
+        parsed = parseLines content Nothing
     print parsed
