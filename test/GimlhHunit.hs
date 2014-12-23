1b71c39ee1a962d40ad51306bce70fad2e8e45b2
Alexey Gaziev <alex.gaziev@gmail.com>
2014-12-22 16:18:32 -0800

    Test for list

--- test/GimlhHunit.hs
+++ test/GimlhHunit.hs
@@ -1,9 +1,20 @@
 import Gimlh
 import Test.HUnit
 
-testParseStringAssertion :: Assertion
-testParseStringAssertion =
+testParseStringWTextAssertion :: Assertion
+testParseStringWTextAssertion =
     let node = ("this_is_text", TextG, Text $ "abc")
         giml = [node]
     in
       giml @=? parseString ":text: this_is_text\nabc"
+
+testParseStringWListAssertion :: Assertion
+testParseStringWListAssertion =
+    let node = ("this_is_list", ListG, List $ ["a", "b", "c"])
+        giml = [node]
+    in
+      giml @=? parseString ":list: this_is_list\na, b, c"
+
+testAll = do
+    testParseStringWTextAssertion
+    testParseStringWListAssertion
